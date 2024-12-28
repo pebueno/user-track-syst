0c33ac7fb91209d39a8f3c113102bd25bb491963
Pedro Ivo Bueno Sartorio <pedroivobu@gmail.com>
2024-12-28 20:43:23 -0300

    Implement multipage structure and add UserDetails page

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
 import { Box } from "@mui/material";
 import Home from "./home";
+import UserDetails from "./userDetails";
 
 const App = () => {
   return (
     <Box sx={{ maxWidth: 800, margin: "80px auto" }}>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users/:id" element={<UserDetails />} />
+        </Routes>
+      </BrowserRouter>
     </Box>
   );
 };
---
+++ client/src/components/userDetails/index.tsx
@@ -0,0 +1,14 @@
+import {Typography } from "@mui/material";
+
+const UserDetails = () => {
+
+  return (
+    <>
+      <Typography variant="h2" gutterBottom>
+        User details
+      </Typography>
+    </>
+  );
+};
+
+export default UserDetails;

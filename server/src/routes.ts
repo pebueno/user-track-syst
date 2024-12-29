9eaa72e0c888fbf40f468d4e70e5a4bbf91b1cb9
Pedro Ivo Bueno Sartorio <pedroivobu@gmail.com>
2024-12-28 21:55:20 -0300

    Create route to Update User

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -21,21 +21,48 @@ router.get("/users/:id", (req: Request, res: Response) => {
   const user = db.prepare("SELECT * FROM users WHERE id = ?").get(id);
 
   if (!user) {
-    res.status(400).json({ error: "User not found"})
+    res.status(404).json({ error: "User not found" });
   }
 
   res.json(user);
 });
 
+router.put("/users/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { first_name, last_name, email, birthday } = req.body;
+
+  if (!first_name || !last_name || !email || !birthday) {
+    res.status(404).json({ error: "All fields are required" });
+    return;
+  }
+
+  const result = db
+    .prepare(
+      "UPDATE users SET first_name = ?, last_name = ?, email = ?, birthday = ? WHERE id = ?",
+    )
+    .run(first_name, last_name, email, birthday, id);
+
+  if (result.changes === 0) {
+    res.status(404).json({ error: "User not found" });
+  }
+
+  res.json({ message: "User updated successfully" });
+});
+
 router.post("/users", (req: Request, res: Response) => {
-  if (!req.body.firstName || !req.body.lastName || !req.body.email || !req.body.birthday) {
+  if (
+    !req.body.firstName ||
+    !req.body.lastName ||
+    !req.body.email ||
+    !req.body.birthday
+  ) {
     res.sendStatus(400);
     return;
   }
 
   const user = db
     .prepare(
-      "INSERT INTO users (first_name, last_name, email, birthday) VALUES (@firstName, @lastName, @email, @birthday)"
+      "INSERT INTO users (first_name, last_name, email, birthday) VALUES (@firstName, @lastName, @email, @birthday)",
     )
     .run({
       firstName: req.body.firstName,
@@ -58,7 +85,7 @@ router.post(
     console.log(file);
 
     res.sendStatus(200);
-  }
+  },
 );
 
 export default router;
